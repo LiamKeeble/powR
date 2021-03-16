a1bb708905bf7d673b6e34fb53d336d311117a2f
Liam Keeble <liamkeeble@gmail.com>
2021-03-16 08:51:21 +0000

    pack

--- R/sims.R
+++ R/sims.R
@@ -7,6 +7,7 @@
 #' @export
 
 lmSim=function(n,f){
+options(scipen=999)
 y=c(rnorm(n=n/2, mean=0, sd=1), rnorm(n=n/2,mean=f,sd=1))
 condition=c(rep("control",times=n/2),rep("experimental",times=n/2))
 m=lm(y~condition)
@@ -15,19 +16,50 @@ return(summary(m))
 }
 
 
-#' Power simulation for a model function
+#'Simulation for a linear model that returns only p values
+#' @param n sample size
+#' @param f effect size
+#' @return p-value for effect
+#' @export
+
+lmP=function(n,f){
+options(scipen=999)
+y=c(rnorm(n=n/2, mean=0, sd=1), rnorm(n=n/2, mean=f, sd=1))
+condition=c(rep("control",times=n/2),rep("experimental",times=n/2))
+m=lm(y~condition)
+return(summary(m)$coefficients[2,4])
+}
+
+
+
+#' Average p value simulation for a model function
 #' @param mod A model simulation
-#' @return Value of statistical power
+#' @return P-values across simulations
 #' @export
 
 
-powSim=function(mod){
+pSim=function(mod){
 output=NULL
 for (i in 1:100){
 output[i]=mod
 return(output)
 }
 }
+
+
+
+#' Power simulation
+#' @param pSim p value simulation
+#' @return Statistical power value for model
+#' @export
+
+power=function(pSim){
+p=pSim
+power=mean(p<0.05)
+power
+}
+
+
 
 
 
