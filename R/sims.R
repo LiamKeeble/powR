4e99bdfed7c208524dea43c9120bc0b89ee71f51
Liam Keeble <liamkeeble@gmail.com>
2021-03-17 16:37:15 +0000

    pow

--- R/sims.R
+++ R/sims.R
@@ -65,6 +65,22 @@ return(summary(m)$coefficients[2,4])
 }
 
 
+
+#' Model simulation for meta analysis random effects model
+#' @param n sample size
+#' @param f effect size
+#' @return p value for model simulation
+#' @export
+
+metaRsim=function(n,f,var){
+options(scipen=999)
+y=rnorm(n,f,1)
+var=rexp(n,var)
+m=metafor::rma.mv(y,var,method="REML")
+return(summary(m)$pval)
+}
+
+
 #' Power simulation for binomial generalised linear model with two level categorical predictor
 #' @param n sample size
 #' @param f effect size
@@ -78,6 +94,24 @@ for (i in 1:iter){
 output[i]=binGlmP(n=n,f=f)
 }
 
+p=output
+power=mean(p<0.05)
+power
+}
+
+#' Power simulation for random effects meta analysis model
+#' @param n sample size
+#' @param f effect size
+#' @param var variance for meta analysis model
+#' @param iter number of iterations of simulation
+#' @return statistical power for meta analysis
+#' @export
+
+powerMetaR=function(n,f,var,iter){
+output=NULL
+for (i in 1:iter){
+output[i]=metaRsim(n=n,f=f,var=var)
+}
 p=output
 power=mean(p<0.05)
 power
@@ -98,6 +132,30 @@ output[i]=binGlmP(n=n,f=f)
 }
 output=data.frame(output)
 
+plot=ggplot2::ggplot(output, aes(output))+
+	geom_density()+
+	ggtitle("Density plot of p-values")+
+	xlab("P values")+
+	ylab("Frequency of values")+
+	geom_vline(aes(xintercept=0.05))
+
+plot
+}
+
+#' Plot distribution of p values from meta analysis random effects model 
+#' @param n sample size
+#' @param f effect size
+#' @param var variance for meta analysis model
+#' @param iter number of iterations of simulation
+#' @return plot of distribution of p values
+#' @export
+
+powMetaRPlot=function(n,f,var,iter){
+output=NULL
+for (i in 1:iter){
+output[i]=metaRsim(n=n,f=f,var=var)
+}
+output=data.frame(output)
 plot=ggplot2::ggplot(output, aes(output))+
 	geom_density()+
 	ggtitle("Density plot of p-values")+
